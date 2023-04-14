b4926f2be232dd3a4c71e4c13d713692038b82eb
rennavittorio <renna.vittorio@gmail.com>
2023-04-14 17:44:57 +0200

    add: add all detail info to single card style: style card

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,13 +2,23 @@
 
 @section('content')
     
-    <div class="">
+    <div class="d-flex flex-column justify-content-center align-items-center">
         
-        <ul>
-            <li>{{ $comic['title'] }}</li>
-        </ul>
+        <div class="card text-center">
+            <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{ $comic['title'] }}</h5>
+              <p class="card-text description">{{ $comic['description'] }}</p>
+              <div class="text-wrapper d-flex justify-content-center gap-3">
+                  <p class="card-text">{{ $comic['price'] }} €</p>
+                  <p class="card-text">{{ $comic['series'] }}</p>
+                  <p class="card-text">{{ $comic['sale_date'] }}</p>
+                  <p class="card-text">{{ $comic['type'] }}</p>
+              </div>
+            </div>
+        </div>
 
-        <div>
+        <div class="mt-3">
             <a href="{{ route('comic.index') }}">
                 Go back to list
             </a>
