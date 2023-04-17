17c9824908c75cd876c6fe69252769900ef4c6ab
rennavittorio <renna.vittorio@gmail.com>
2023-04-17 14:50:34 +0200

    add: add route to store edit: edit creation form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,51 +3,53 @@
 @section('content')
 
 <h1>
-    Insert your comic
+    Insert new comic
 </h1>
     
 <form class="row g-3">
-    <div class="col-md-6">
-        <label for="inputEmail4" class="form-label">Email</label>
-        <input type="email" class="form-control" id="inputEmail4">
-    </div>
-    <div class="col-md-6">
-        <label for="inputPassword4" class="form-label">Password</label>
-        <input type="password" class="form-control" id="inputPassword4">
-    </div>
+
     <div class="col-12">
-        <label for="inputAddress" class="form-label">Address</label>
-        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+        <label for="title" class="form-label">Title</label>
+        <input type="text" class="form-control" id="title" name="title">
     </div>
+
     <div class="col-12">
-        <label for="inputAddress2" class="form-label">Address 2</label>
-        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+        <label for="description" class="form-label">Description</label>
+        <textarea class="form-control" id="description" name="description"></textarea>
     </div>
-    <div class="col-md-6">
-        <label for="inputCity" class="form-label">City</label>
-        <input type="text" class="form-control" id="inputCity">
+
+    <div class="col-12">
+        <label for="src" class="form-label">src</label>
+        <input type="text" class="form-control" id="src" name="src">
     </div>
-    <div class="col-md-4">
-        <label for="inputState" class="form-label">State</label>
-        <select id="inputState" class="form-select">
-            <option selected>Choose...</option>
-            <option>...</option>
+
+    <div class="col-6">
+        <label for="price" class="form-label">price</label>
+        <input type="number" step="0.01" class="form-control" id="price" name="price">
+    </div>
+
+    <div class="col-6">
+        <label for="series" class="form-label">series</label>
+        <input type="text" class="form-control" id="series" name="series">
+    </div>
+
+    <div class="col-6">
+        <label for="sale_date" class="form-label">sale date</label>
+        <input type="date" class="form-control" id="sale_date" name="sale_date">
+    </div>
+
+    <div class="col-6">
+        <label for="type" class="form-label">type</label>
+        <select class="form-select" aria-label="Default select example" id="type" name="type">
+            <option selected>Open this select menu</option>
+            <option value="comic book">comic book</option>
+            <option value="graphic novel">graphic novel</option>
         </select>
     </div>
-    <div class="col-md-2">
-        <label for="inputZip" class="form-label">Zip</label>
-        <input type="text" class="form-control" id="inputZip">
-    </div>
+
+    {{-- btn --}}
     <div class="col-12">
-      <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="gridCheck">
-            <label class="form-check-label" for="gridCheck">
-                Check me out
-            </label>
-      </div>
-    </div>
-    <div class="col-12">
-        <button type="submit" class="btn btn-primary">Sign in</button>
+        <button type="submit" class="btn btn-primary">Save</button>
         <div class="btn btn-warning">
             <a href="{{ route('comics.index') }}">
                 Go back to list
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
 
 Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
+
+Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
